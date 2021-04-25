0816d3956b067fae5b73a6e73a8789233b5aeb86
Kevin <zuoyenan@gmail.com>
2021-04-25 09:39:20 +0800

    add: rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the Number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    println!("Secret Number is {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        match io::stdin()
+            .read_line(&mut guess) {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Failed to read line!");
+                    continue;
+                },
+            };
+
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
+        };  
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        
+
+    }
+    
 
 }
